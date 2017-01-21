c23daebe18af1ce888cc8f6dcf3e4ab893a6cde7
IlalaSwinGolf <IlalaSwinGolf@users.noreply.github.com>
2017-01-21 17:43:59 +0100

    Reusable Bookshelf instance

---
+++ bookshelf.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV]);
+
+const bookshelf = require('bookshelf')(knex);
+
+bookshelf.plugin('registry'); 
+
+module.exports = bookshelf;
