d67537afeb8c2856df9e92238c74d5300d841616
IlalaSwinGolf <IlalaSwinGolf@users.noreply.github.com>
2017-02-25 11:08:27 +0100

    Roles test: green

--- routes/role.js
+++ routes/role.js
@@ -5,7 +5,6 @@ const roleController = require('../controllers/role');
 const apiRoutes = express.Router();
 
 apiRoutes.get('/', roleController.getRoles);
-apiRoutes.get('/:id', roleController.findById);
 apiRoutes.all('*', roleController.forbidden);
 
 module.exports = apiRoutes;
