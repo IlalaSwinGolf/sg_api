2fe3c162d4d52ce6ff23a389f6622c8a4021ee21
IlalaSwinGolf <IlalaSwinGolf@users.noreply.github.com>
2017-03-04 18:25:26 +0100

    Change user migration

--- db/migrations/20170121173624_create_users_table.js
+++ db/migrations/20170121173624_create_users_table.js
@@ -2,10 +2,10 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTableIfNotExists('users', function(table) {
         table.increments('id').unsigned().primary();
         table.integer('role_id').unsigned().index().references('id').inTable('roles').onDelete("CASCADE").notNullable();
-        table.string('login').unique().notNullable();
+        table.string('username').unique().notNullable();
         table.string('email').unique().notNullable();
         table.string('password').notNullable();
-        table.boolean('disabled').defaultTo(false).notNullable();
+        table.boolean('disabled').defaultTo(false);
         table.timestamps();
     });
 };
