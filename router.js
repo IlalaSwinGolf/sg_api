1ce44927225e0255687cd6cab0680646b7f08999
IlalaSwinGolf <IlalaSwinGolf@users.noreply.github.com>
2017-03-04 18:29:00 +0100

    Update routes

--- router.js
+++ router.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const roleController = require('./controllers/role');
-const roleRoutes = require ('./routes/role');
-const userController = require('./controllers/user');
-const userRoutes = require ('./routes/user');
+const Express = require('express');
+const RoleRoutes = require ('./routes/role');
+const UserRoutes = require ('./routes/user');
+const AuthRoutes = require ('./routes/auth');
 const APIVersion =  require ('./api_version')();
+const CustomErrors = require('./helpers/custom-errors');
 
 module.exports = function(app) {
-    const apiRoutes = express.Router();
+    const apiRoutes = Express.Router();
 
-    //routes will go here
-    apiRoutes.use('/roles', roleRoutes);
-    apiRoutes.use('/users', userRoutes);
-    apiRoutes.all('*', (req, res) => res.status(200).send({
-        error: false,
-        message: 'Welcome to swin-golf scorecard application.',
-    }));
+    apiRoutes.use('/roles', RoleRoutes);
+    apiRoutes.use('/users', UserRoutes);
+    apiRoutes.use('/auth' , AuthRoutes);
+    apiRoutes.all('*', (req, res, next) => next(new CustomErrors.routeError(405, req.url + " url route with "  + req.method + " method has not been found ")));
 
     app.use('/api/' + APIVersion, apiRoutes);
 }
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,5 @@ const apiRoutes = express.Router();
 
 apiRoutes.get('/'   , userController.fetchAll);
 apiRoutes.get('/:id', userController.fetch);
-apiRoutes.post('/'   , userController.create);
-apiRoutes.all('*'   , userController.forbidden);
 
 module.exports = apiRoutes;
