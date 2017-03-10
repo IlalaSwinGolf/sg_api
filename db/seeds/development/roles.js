080dc65021e74347787909fa113c72e0df8f2abb
IlalaSwinGolf <IlalaSwinGolf@users.noreply.github.com>
2017-03-10 20:07:06 +0100

    Remove explicit ids from seed files

--- db/seeds/development/roles.js
+++ db/seeds/development/roles.js
@@ -2,8 +2,8 @@
 
 exports.seed = function(knex, Promise) {
   return knex('roles').del()
-    .then(function () { return knex('roles').insert({id: 1, status: 'root'})})
-    .then(function () { return knex('roles').insert({id: 2, status: 'admin'})})
-    .then(function () { return knex('roles').insert({id: 3, status: 'user'})})
-    .then(function () { return knex('roles').insert({id: 4, status: 'guest'})})
+    .then(function () { return knex('roles').insert({status: 'root'})})
+    .then(function () { return knex('roles').insert({status: 'admin'})})
+    .then(function () { return knex('roles').insert({status: 'user'})})
+    .then(function () { return knex('roles').insert({status: 'guest'})})
 };
--- db/seeds/test/users.js
+++ db/seeds/test/users.js
@@ -2,8 +2,8 @@
 
 exports.seed = function(knex, Promise) {
   return knex('users').del()
-    .then(function () { return knex('users').insert({id: 1, role_id: 1, username: 'flocateur', email: 'flocateur@gmail.com', password: 'pwd'})})
-    .then(function () { return knex('users').insert({id: 2, role_id: 1, username: 'amaujito' , email: 'amaujito@gmail.com' , password: 'pwd'})})
-    .then(function () { return knex('users').insert({id: 3, role_id: 1, username: 'carlito'  , email: 'carlito@gmail.com'  , password: 'pwd'})})
-    .then(function () { return knex('users').insert({id: 4, role_id: 1, username: 'maryam'   , email: 'maryam@gmail.com'   , password: 'pwd'})})
+    .then(function () { return knex('users').insert({role_id: 1, username: 'flocateur', email: 'flocateur@gmail.com', password: 'pwd'})})
+    .then(function () { return knex('users').insert({role_id: 1, username: 'amaujito' , email: 'amaujito@gmail.com' , password: 'pwd'})})
+    .then(function () { return knex('users').insert({role_id: 1, username: 'carlito'  , email: 'carlito@gmail.com'  , password: 'pwd'})})
+    .then(function () { return knex('users').insert({role_id: 1, username: 'maryam'   , email: 'maryam@gmail.com'   , password: 'pwd'})})
 };
