9e206ccc9de34bf6cf93e8b82259906ab99902a3
IlalaSwinGolf <IlalaSwinGolf@users.noreply.github.com>
2017-03-12 18:23:45 +0100

    Clean migrations

--- db/migrations/20170121173624_create_users_table.js
+++ db/migrations/20170121173624_create_users_table.js
@@ -3,7 +3,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTableIfNotExists('users', function(table) {
         table.increments('id').unsigned().primary();
-        table.integer('role_id').unsigned().index().references('id').inTable('roles').onDelete("CASCADE").notNullable();
+        table.integer('role_id').unsigned().index().references('id').inTable('roles').onDelete("CASCADE").defaultTo(3);
         table.string('username').unique().notNullable();
         table.string('email').unique().notNullable();
         table.string('password').notNullable();
